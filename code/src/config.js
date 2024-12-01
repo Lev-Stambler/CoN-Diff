a25024fb394699ab5fd355b5b2ed7672bddc891c
Lev Stambler <levstamb@gmail.com>
2024-12-02 04:18:04 +0700

    rm

--- code/src/config.js
+++ code/src/config.js
@@ -3,6 +3,7 @@ const config = {
   PORT: 3000,
   COMPUTATION_ID: 'sum_example',
   FIELD_SIZE: BigInt('340282366920938463463374607431768211507'), // 128-bit prime
+  //FIELD_SIZE: BigInt('340282366920938463463374607431768211507'), // 128-bit prime
   N_PARTIES: 3, // Number of parties in the MPC computation
 };
 
