399b6560ae1fbb9841cbb7aa574d45abaa42a9cd
Lev Stambler <levstamb@gmail.com>
2024-12-08 23:12:54 +0700

    works!

--- code/src/aggregator.js
+++ code/src/aggregator.js
@@ -60,7 +60,9 @@ const aggregate = async (n_parties, prf_input) => {
 	const reses = await Promise.all(new Array(n_parties).fill(0).map(async (_, i) => {
 		await perPartyVer(mat, i + 1)
 	}))
-	assert(reses.every(r => r[0]), "Some parties failed")
+	if (!reses.every(r => r[0])){
+		throw "Some parties failed"
+	}
 	const outputs = reses.map(r => r[1])
 	const agg_output = outputs.reduce((a, b) => a + b, 0n)
 
